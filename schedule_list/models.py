440a1c6da251377a8d3ee0f19bb7bf8d6a45c1d3
H J <qkrwlgml33@gmail.com>
2024-01-05 18:19:28 +0900

    guestspace table 추가

--- schedule_list/models.py
+++ schedule_list/models.py
@@ -10,4 +10,6 @@ class Schedule(models.Model):
     update_at = models.DateTimeField(auto_now=True)
 
 
-class 
+class GuestSpace(models.Model):
+    Schedule_id = models.ForeignKey("schedule_list.Schedule", on_delete=models.CASCADE, db_column="schedule_list_id")
+    user_id = models.ForeignKey("user.User", on_delete=models.CASCADE, db_column="user_id")
