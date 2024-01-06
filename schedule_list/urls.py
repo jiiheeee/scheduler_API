38271a07e49069651804d1b68403265a97e1492a
H J <qkrwlgml33@gmail.com>
2024-01-06 15:28:54 +0900

    스케줄 목록 urls 추가

--- schedule_list/urls.py
+++ schedule_list/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ScheduleAddView
+from .views import ScheduleAddView, ScheduleListView
 
 urlpatterns = [
     path("add/", ScheduleAddView.as_view()),
+    path("<int:user_id>", ScheduleListView.as_view())
 ]
