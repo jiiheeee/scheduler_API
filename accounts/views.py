3cf33046718b5c661695988250bd5ddbd780d38a
H J <qkrwlgml33@gmail.com>
2024-01-06 15:27:19 +0900

    이메일 중복 회원가입 불가 코드 수정'

--- accounts/views.py
+++ accounts/views.py
@@ -3,22 +3,23 @@ from user.models import User
 from rest_framework.views import APIView
 from user.models import User
 
-
+# ToDo: 로그인
+# 
 class SignUpView(APIView):
     def post(self, request):
         name = request.data.get("name")
         password = request.data.get('password')
         mail = request.data.get('mail')
 
-        User.objects.create(
-            user_name=name, 
-            password=password, 
-            mail=mail, 
-            is_active=True
-        )
-        return HttpResponse('회원가입이 완료되었습니다.')
-    
+        existing_user = User.objects.filter(mail=mail).first()
 
-
-
-    
+        if existing_user:
+            return HttpResponse("이미 존재하는 이메일입니다.")
+        else:
+            User.objects.create(
+                username=name, 
+                password=password, 
+                mail=mail, 
+                is_active=True
+            )
+            return HttpResponse('회원가입이 완료되었습니다.')
