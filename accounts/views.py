b3a62f66f93d1e3f4b971163db87e03883fcf07f
H J <qkrwlgml33@gmail.com>
2024-01-06 15:50:26 +0900

    LoginView 추가

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse
 from user.models import User
 from rest_framework.views import APIView
 from user.models import User
-
-# ToDo: 로그인
-# 
+from user.serializers import UserSerializer
+from rest_framework.response import Response
+ 
 class SignUpView(APIView):
     def post(self, request):
         name = request.data.get("name")
@@ -23,3 +23,16 @@ class SignUpView(APIView):
                 is_active=True
             )
             return HttpResponse('회원가입이 완료되었습니다.')
+        
+class LoginView(APIView):
+    def post(self, request):
+        user_email = request.data.get("email")
+        user_password = request.data.get("password")
+
+        try:
+            login_data = User.objects.get(mail=user_email, password=user_password)
+            if login_data:
+                serializers = UserSerializer(login_data)
+                return Response(serializers.data)
+        except User.DoesNotExist:
+            return HttpResponse("이메일 혹은 비밀번호를 확인해주세요.", status=400)
